c84287974c12ee9dd5fef1c75164ed0988062fb4
Jarnomer <jankkumer@pm.me>
2024-06-27 19:00:59 +0300

    Testing depth detection for sprite drawing

--- bonus/srcs/draw/sprite_bonus.c
+++ bonus/srcs/draw/sprite_bonus.c
@@ -24,36 +24,37 @@ static int32_t	calculate_shade(float dy)
 	return (get_rgba(0, 0, 0, intensity));
 }
 
-static void	draw_pixels(t_camera *sprite, t_camera *texture, t_cubed *game)
+static void	draw_sprite(float *depth, t_camera *spr, t_camera *tex, t_cubed *game)
 {
 	int			screen[2];
 	int32_t		color;
 	int32_t		shade;
 	mlx_image_t	*img;
 
-	img = game->asset[(int)sprite->a];
-	screen[X] = sprite->x - sprite->dx / 2 - 1;
-	while (++screen[X] < sprite->x + sprite->dx / 2)
+	img = game->asset[(int)spr->a];
+	screen[X] = spr->x - spr->dx / 2 - 1;
+	while (++screen[X] < spr->x + spr->dx / 2)
 	{
 		screen[Y] = -1;
-		texture->y = img->height;
-		while (++screen[Y] < sprite->dy)
+		tex->y = img->height;
+		printf("screen x = %d | wall depth = %f | sprite dy = %f\n", screen[X], depth[10], spr->dy);
+		while (++screen[Y] < spr->dy)
 		{
-			color = get_pixel_color(img, texture->x, texture->y);
-			texture->y -= texture->dy;
+			color = get_pixel_color(img, tex->x, tex->y);
 			if (get_channel_color(color, GET_ALPHA))
 			{
-				shade = calculate_shade(sprite->dy);
+				shade = calculate_shade(spr->dy);
 				color = get_alpha_blend(shade, color);
 			}
-			if (ft_valid_pixel(game->asset[IMG_OL], screen[X], sprite->y - screen[Y]))
-				mlx_put_pixel(game->asset[IMG_OL], screen[X], sprite->y - screen[Y], color);
+			if (ft_valid_pixel(game->asset[IMG_OL], screen[X], spr->y - screen[Y]))
+				mlx_put_pixel(game->asset[IMG_OL], screen[X], spr->y - screen[Y], color);
+			tex->y -= tex->dy;
 		}
-		texture->x += texture->dx;
+		tex->x += tex->dx;
 	}
 }
 
-static void	draw_sprite(int map[2], t_camera *cam, t_cubed *game)
+static void	process_sprite(int map[2], float *depth, t_camera *cam, t_cubed *game)
 {
 	static int	sprite_limit = 5000;
 	t_camera	texture;
@@ -64,16 +65,35 @@ static void	draw_sprite(int map[2], t_camera *cam, t_cubed *game)
 	if (!sprite.dy || ft_in_sprite(map, cam, game)
 		|| sprite.dy >= sprite_limit)
 		return ;
-	draw_pixels(&sprite, &texture, game);
+	draw_sprite(depth, &sprite, &texture, game);
+}
+
+static void	calc_spr_walls(float *depth, t_camera *cam, float angle, t_cubed *game)
+{
+	t_vector	ray;
+	int			column;
+
+	column = 0;
+	ft_rotate(&angle, FOV / 2, ROTATE_LEFT);
+	while (column < SCREEN_WIDTH)
+	{
+		ray.a = angle;
+		calculate_ray(&ray, cam, game);
+		ft_rotate(&angle, STEP_WINDOW, ROTATE_RIGHT);
+		depth[column] = ray.d;
+		column++;
+	}
 }
 
 void	draw_sprites(t_camera *cam, float angle, t_cubed *game)
 {
+	float	depth[SCREEN_WIDTH];
 	int		map[2];
 	char	c;
 
 	(void)angle;
 	map[X] = 0;
+	calc_spr_walls(depth, cam, cam->a, game);
 	while (map[X] < game->map->width)
 	{
 		map[Y] = 0;
@@ -81,7 +101,7 @@ void	draw_sprites(t_camera *cam, float angle, t_cubed *game)
 		{
 			c = get_map_element(map[X], map[Y], game);
 			if (c == MAP_HEALTH || c == MAP_AMMO)
-				draw_sprite(map, cam, game);
+				process_sprite(map, depth, cam, game);
 			map[Y]++;
 		}
 		map[X]++;
